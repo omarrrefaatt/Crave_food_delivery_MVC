defd89a85500d9872a3421339d28d816ffdd1b86
Ziad Muhamed <Ziad.Ibrahim.FCI22284@sadatacademy.edu.eg>
2025-05-02 16:43:25 +0300

    fooditems crud fixed

--- backend/Crave.API/models/Responses/UserResponse.cs
+++
@@ -1,7 +0,0 @@
-public class UserResponse
-{
-    public int Id { get; set; }
-    public string Username { get; set; }
-    public string Email { get; set; }
-    public string Token { get; set; }  
-}
